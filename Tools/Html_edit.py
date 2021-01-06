f87e86f0996e73acfec9af5379965926f64dcedf
naveeng2402 <77045663+naveeng2402@users.noreply.github.com>
2021-01-06 22:14:52 +0530

    Update Html_edit.py

--- Tools/Html_edit.py
+++ Tools/Html_edit.py
@@ -1,6 +1,11 @@
-with open('text.txt') as f:
-    x = f.readlines()
-    x = [i.strip().replace('''"''', "'") for i in x]
-    x = '\\n'.join(x)
-
+try:
+    with open('text.txt') as f:
+        x = f.readlines()
+        x = [i.strip().replace('''"''', "'") for i in x]
+        x = '\\n'.join(x)
+except:
+    with open('Tools/text.txt') as f:
+        x = f.readlines()
+        x = [i.strip().replace('''"''', "'") for i in x]
+        x = '\\n'.join(x)
 print(x)
